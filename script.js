84c1b45d72d717f18282dc8b88673dbe277a68f4
sanikayawale <sanikayawale@gmail.com>
2024-04-27 09:14:48 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// let products={
+//     data:[
+//         {
+//         pname:"Mahindra 265 DI",
+//         category:"Tractor",
+//         price: "Rs.5,00,000",
+//         image: "mahindra-265-di-tractor.jpg",
+//     },
+//     {
+//         pname:"Mahindra 265 DI",
+//         category:"Tractor",
+//         price: "Rs.5,00,000",
+//         image: "mahindra-265-di-tractor.jpg",
+//     },
+//     {
+//         pname:"Mahindra 265 DI",
+//         category:"Tractor",
+//         price: "Rs.5,00,000",
+//         image: "mahindra-265-di-tractor.jpg",
+//     },
+//     {
+//         pname:"Mahindra 265 DI",
+//         category:"Tractor",
+//         price: "Rs.5,00,000",
+//         image: "mahindra-265-di-tractor.jpg",
+//     },
+//     {
+//         pname:"Mahindra 265 DI",
+//         category:"Tractor",
+//         price: "Rs.5,00,000",
+//         image: "mahindra-265-di-tractor.jpg",
+//     },
+//     {
+//         pname:"Mahindra 265 DI",
+//         category:"Tractor",
+//         price: "Rs.5,00,000",
+//         image: "mahindra-265-di-tractor.jpg",
+//     },
+//     {
+//         pname:"Mahindra 265 DI",
+//         category:"Tractor",
+//         price: "Rs.5,00,000",
+//         image: "mahindra-265-di-tractor.jpg",
+//     },
+//     {
+//         pname:"Mahindra 265 DI",
+//         category:"Tractor",
+//         price: "Rs.5,00,000",
+//         image: "mahindra-265-di-tractor.jpg",
+//     },
+//     {
+//         pname:"Mahindra 265 DI",
+//         category:"Tractor",
+//         price: "Rs.5,00,000",
+//         image: "mahindra-265-di-tractor.jpg",
+//     },
+// ],
+// };
+
+// for (let i of products.data) {
+//     let card = document.createElement("div");
+//     card.classList.add("card", "i.category");
+//     let imgContainer=doc.createElement("div");
+//     imagecontainer.classList.add("image-container");
+//     let image=document.createElement("img");
+//     image.setAttribute("src", i.image);
+//     imgContainer.appendChild(image);
+//     card.appendChild(imgContainer);
+
+//     document.getElementById("products").appendChild(card);
+// }
